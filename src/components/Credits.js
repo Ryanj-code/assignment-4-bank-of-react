40e82e32c39b3c4a0125afe95b6bbf34a725a722
Ryan Jiang <jiangr0421@gmail.com>
2023-04-16 20:30:56 -0400

    Added account balance to both credits and debits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,6 +5,7 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
   // Create the list of Credit items
@@ -39,6 +40,7 @@ const Credits = (props) => {
         <button type="submit">Add Credit</button>
       </form>
 
+      <AccountBalance accountBalance={props.accountBalance}/>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
